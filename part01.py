fa4edd4cd557e4b0b9cecf46a03ba8fae5ad74b1
garum3075 <garum3075@gmail.com>
2025-12-22 15:51:56 +0800

    part01.py

--- part01.py
+++ part01.py
@@ -1,50 +1,48 @@
-# 1. 定義位置(視窗、人物位置比例)
-# 2. 設定class+定義draw後達到可以重複使用
+import pygame
 
-# import pygame
-
-# pygame.init()
+pygame.init()
 
 
-# SCREEN_WIDTH = 800
-# SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
-# screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-# pygame.display.set_caption('射擊遊戲(取自YouTube Coding With Russ)')
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption('射擊遊戲(取自YouTube Coding With Russ)')
 
 
-# class Soldier(pygame.sprite.Sprite): #explain-124
-#     def __init__(self, x, y, scale):
-#         pygame.sprite.Sprite.__init__(self)
-#         img = pygame.image.load('img/player/Idle/0.png') #玩家小綠人
-#         self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale) )) # 圖片尺寸調整 explain-78
-#         self.rect = self.image.get_rect()
-#         self.rect.center = (x, y) # 確保物體在正中間
+class Soldier(pygame.sprite.Sprite): #explain-124
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/player/Idle/0.png') #玩家小綠人
+        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale) )) # 圖片尺寸調整 explain-78
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y) # 確保物體在正中間
     
-#     def draw(self): # 自己可以重複使用 像是player.draw()
-#         screen.blit(self.image, self.rect) #explain-65
+    def draw(self): # 自己可以重複使用 像是player.draw()
+        screen.blit(self.image, self.rect) #explain-65
 
 
 
-# player =Soldier(200, 200, 3)
-# player2 =Soldier(400, 200, 3)
+player =Soldier(200, 200, 3)
+player2 =Soldier(400, 200, 3)
 
-# run = True # explain-1
-# while run:
+run = True # explain-1
+while run:
 
 
 
     
-#     player.draw()
-#     player2.draw()
+    player.draw()
+    player2.draw()
 
-#     for event in pygame.event.get():
-#         # quit game
-#         if event.type == pygame.QUIT:
-#             run = False
+    for event in pygame.event.get():
+        # quit game
+        if event.type == pygame.QUIT:
+            run = False
 
 
-#     pygame.display.update() # explain-54
+    pygame.display.update() # explain-54
+
 
 
 # pygame.quit()
